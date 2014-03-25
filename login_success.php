528eedd4d0e78adb95aee7d504f2d1d529a22ce9
magiclud <you@example.com>
2014-03-25 20:16:10 +0100

    skrypt

--- login_success.php
+++ login_success.php
@@ -1,17 +1,14 @@
-// Check if session is not registered, redirect back to main page.
-// Put this code in first line of web page.
-// <?php
-// session_start();
-// if(!session_is_registered(myusername)){
-// header("location:main_login.php");
-// }
-// ?>
 
 <html>
 <body>
 Login Successful
 <h1>Strona g³ówna</h1>h1>
 <?php 
+// session_start();
+// if(!session_is_registered(myusername)){
+// header("location:main_login.php");
+// }
+//
 	if(isset($_SESSION['prawid_uzyt'])){
 		echo 'U¿ytkownik zalaogowany jako: '.$_SESSION['prawid_uzyt'].'<br />';
 		echo '<a href="logout.php">Wylogowanie</a><br />';
